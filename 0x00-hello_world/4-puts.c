7c3e3ac2dfab1c184204df652cf30d226e8ff896
ikashi onyije <ikashionyije@gmail.com>
2022-12-08 03:34:42 -0800

    Almost there

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+char string[] = "Programming is like building a multilingual puzzle";
+
+puts(string);
+
+return 0;
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,9 +5,11 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,");
+
 Return 0;
 }
