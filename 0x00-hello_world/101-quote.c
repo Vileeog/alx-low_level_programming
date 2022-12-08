fa8e362cbce449e6c636eea6474d990e697cb162
ikashi onyije <ikashionyije@gmail.com>
2022-12-08 07:27:27 -0800

    Done for the day

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+/**
+ * main - Entry point
+ * 
+ * Return: 1 (Error)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+return (1)
+}
