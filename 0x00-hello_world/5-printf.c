4775684c59d4b63d285d0cb2ddebddfa60b5cf23
ikashi onyije <ikashionyije@gmail.com>
2022-12-08 02:47:25 -0800

    using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,13 @@
-#!/bin/bash
+/**
+*main - Entry point
+*
+*Return: always 0 (success)
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
 printf("with proper grammar, but the outcome is a piece of art,");
+Return 0;
+}
