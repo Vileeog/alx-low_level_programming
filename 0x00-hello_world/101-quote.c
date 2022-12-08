a2a284cdfbc67230df1826841d0d0f8f2c07fbaf
ikashi onyije <ikashionyije@gmail.com>
2022-12-08 07:58:31 -0800

    I am done with betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 /**
  * main - Entry point
- * 
+ *
  * Return: 1 (Error)
  */
 
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-return (1)
+return (1);
 }
