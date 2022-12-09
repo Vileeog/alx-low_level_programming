36f609d6a80548ef11fb3642622a30f098ab0ca7
ikashi onyije <ikashionyije@gmail.com>
2022-12-09 10:10:47 -0800

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,11 +16,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 printf("Last digit of %d is %d ", n, n % 10);
 if (n % 10 > 5)
-printf("%d is greater than 5\n", n);
+printf("and is greater than 5\n", n);
 else if (n % 10 == 0)
-printf("%d is equal to 0\n", n);
-else if (n % 10 < 6)
-printf("%d is less than 6\n", n);
+printf("and is 0\n", n);
+else if (n % 10 < 6 && n % 10 != 0)
+printf("and is less than 6 and 0\n", n);
 
 return (0);
 }
