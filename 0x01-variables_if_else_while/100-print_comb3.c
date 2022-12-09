a65eebaed77a930ff6b83506501532137028154b
ikashi onyije <ikashionyije@gmail.com>
2022-12-09 15:25:47 -0800

    Almost there

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,24 +8,24 @@
 
 int main(void)
 {
-  int x, y;
+int x, y;
 
-  for (x = 48; x <= 56; x++)
-    {
-      for (y = 49; y <= 57; y++)
-	{
-	  if (y > x)
-	    {
-	      putchar(x);
-	      putchar(y);
-	      if (x != 56 || y != 57)
-		{
-		  putchar(',');
-		  putchar(' ');
-		}
-	    }
-	}
-    }
-  putchar('\n');
+for (x = 48; x <= 56; x++)
+{
+for (y = 49; y <= 57; y++)
+{
+if (y > x)
+{
+putchar(x);
+putchar(y);
+if (x != 56 || y != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
 return (0);
 }
