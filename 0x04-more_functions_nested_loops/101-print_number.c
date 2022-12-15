5a734775a1ee4fac79bf476b3a117b42a66a599d
ikashi onyije <ikashionyije@gmail.com>
2022-12-15 14:26:18 -0800

    Nested functions have loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ */
+
+void print_number(int n)
+{
+unsigned int x;
+
+if (n < 0)
+{
+x = -n;
+_putchar('-');
+}
+else
+{
+x = n;
+}
+
+if (x / 10)
+{
+print_number(x / 10);
+}
+
+_putchar((x % 10) + '0');
+}
