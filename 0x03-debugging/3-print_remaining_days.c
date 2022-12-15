36e73a7a5f14ccc6faba21a770d580ba60eec9c4
ikashi onyije <ikashionyije@gmail.com>
2022-12-15 10:08:44 -0800

    All bugs removed!

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,7 +13,11 @@
 void print_remaining_days(int month, int day, int year)
 
 {
-  if ((year % 100 == 0 || year % 400 == 0) && !(year % 4 == 0))
+  /**
+   * leap year when it can be divided
+   * by 100 and 400 or by 4 evenly
+   */
+if ((year % 100 == 0 || year % 400 == 0) && !(year % 4 == 0))
 {
 if (month >= 2 && day >= 60)
 {
