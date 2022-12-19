1e0f7e468992b04b404bfae1fc665b522f2f2210
ikashi onyije <ikashionyije@gmail.com>
2022-12-19 15:33:47 -0800

    Strings to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,6 +10,7 @@ int _atoi(char *s)
 {
 int sign = 1;
 unsigned int num = 0;
+int n = num * sign;
 
 do {
 if (*s == '-')
@@ -20,5 +21,5 @@ else if (num > 0)
 break;
 } while (*s++);
 
-return (num * sign);
+return (n);
 }
