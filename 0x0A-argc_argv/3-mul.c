2f5afb80461b90fcf9675cb6832e278c0201724b
ikashi onyije <ikashionyije@gmail.com>
2023-01-06 15:05:25 -0800

    Arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,10 +12,17 @@
 
 int main(int argc, char *argv[])
 {
+int x, y;
+
 if (argc < 3)
 {
 printf("Error\n");
 return (1);
 }
-printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+x = atoi(argv[1]);
+y = atoi(argv[2]);
+
+printf("%d\n", x * y);
 return (0);
+}
