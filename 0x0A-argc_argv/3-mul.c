452eeed88cf00c6b162b996ad55cb9ad20c9b277
ikashi onyije <ikashionyije@gmail.com>
2023-01-06 14:43:08 -0800

    Arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - multiplies two numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * _atoi - converts a string to an integer
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
