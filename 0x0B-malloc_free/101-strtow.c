44e0a7ca76de5dd0b836a8bd1fc00f7092e91458
ikashi onyije <ikashionyije@gmail.com>
2023-01-11 13:44:03 -0800

    Done

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,7 +10,6 @@
 int count_word(char *s)
 {
 int flag, c, n;
-
 flag = 0;
 n = 0;
 
@@ -24,7 +23,6 @@ flag = 1;
 n++;
 }
 }
-
 return (n);
 }
 
@@ -38,41 +36,44 @@ return (n);
 char **strtow(char *str)
 {
 char **mat, *tmp;
-int i, k = 0, len = 0, words, c = 0, start, end;
-
+int i, j, k = 0, len = 0, words, c = 0, start, end;
+if (str == NULL || *str == '\0')
+return (NULL);
 while (*(str + len))
 len++;
 words = count_word(str);
 if (words == 0)
 return (NULL);
-
 mat = (char **) malloc(sizeof(char *) * (words + 1));
 if (mat == NULL)
 return (NULL);
-
 for (i = 0; i <= len; i++)
 {
 if (str[i] == ' ' || str[i] == '\0')
 {
-if (c)
 {
 end = i;
-tmp = (char *) malloc(sizeof(char) * (c + 1))
-if (tmp == NULL)
+tmp = (char *) malloc(sizeof(char) * (c + 1));
+while (--k >= 0)
+free(mat);
 return (NULL);
-while (start < end)
-*tmp++ = str[start++];
-*tmp = '\0';
-mat[k] = tmp - c;
+}
+for (j = 0; j < c; j++)
+tmp[j] = str[start + j];
+tmp[c] = '\0';
+mat[k] = tmp;
 k++;
 c = 0;
 }
 }
-else if (c++ == 0)
+else if (c == 0)
+{
 start = i;
+c++;
 }
-
+else
+c++;
+}
 mat[k] = NULL;
-
 return (mat);
 }
